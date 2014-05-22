1eae0cdde7ed5a99f6aed9fdbc50046a2fd0eece
dr87 <nb00870@gmail.com>
2014-05-22 02:52:22 -0400

    Add rest of variants
    Signed-off-by: dr87 <nb00870@gmail.com>

---
+++ ls980/proprietary/Android.mk
@@ -0,0 +1,15 @@
+ifneq ($(filter g2 d800 d801 d802 d803 vs980 ls980,$(TARGET_DEVICE)),)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := TimeService
+LOCAL_MODULE_OWNER := lge
+LOCAL_SRC_FILES := app/$(LOCAL_MODULE).apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_MODULE_TAGS := optional
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
+include $(BUILD_PREBUILT)
+
+endif
